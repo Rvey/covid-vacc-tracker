1ea39afc8b142837252ca4f7be61247d6a180688
Redouane Bouabana <rredouane342@gmail.com>
2022-02-15 14:49:27 +0100

    Update VerificationForm.tsx

--- FrontEnd/src/components/Forms/VerificationForm.tsx
+++ FrontEnd/src/components/Forms/VerificationForm.tsx
@@ -1,16 +1,12 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import { SetStateAction, useState, useRef } from "react";
 import * as Yup from "yup";
 const VerificationForm = () => {
-  //     const UserSchema = Yup.object().shape({
-  //     delivery: Yup.string().min(2, 'Too Short!').required('Required'),
-  //     region: Yup.string().required('Required'),
-  //     age: Yup.number().required('Required')
-  // });
   return (
     <div>
       <Formik
         initialValues={{
-          age: "",
+          age: 0,
           vaccinated: false,
           effected: "",
         }}
@@ -26,31 +22,27 @@ const VerificationForm = () => {
               <label
                 htmlFor="age"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
-                Age
-              </label>
+              ></label>
               <Field
                 type="number"
                 id="age"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                 name="age"
               />
-              {errors.age && touched.age ? (
-                <div className="text-red-500 font-semibold">{errors.age}</div>
-              ) : null}
             </div>
-            {/* first vacc check */}
-            <div className="mt-4">
-              <label>
-                <h1>are you vaccinated</h1>
-                <Field type="checkbox" name="vaccinated" />
-              </label>
-            </div>
-            {values.vaccinated && (
+
+            {values.age > 12 && (
               <div className="mt-4">
-                <h1>tare you already effected</h1>
+                <label>
+                  <h1>are you vaccinated</h1>
+                  <Field type="checkbox" name="firstVacc" />
+                  <Field type="checkbox" name="secondVacc" />
+                  <Field type="checkbox" name="vaccinated" />
+                </label>
               </div>
             )}
+
+            {values.vaccinated && <div>are u already infected</div>}
 
             <div className="mt-8 flex justify-between">
               <button
