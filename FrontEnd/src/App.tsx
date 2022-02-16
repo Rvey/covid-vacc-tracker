a61ff57276f61d23bcf717da64dd92970f9e653f
Redouane Bouabana <rredouane342@gmail.com>
2022-02-16 09:14:43 +0100

    fix checked input

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
 import VerificationForm from "./components/Forms/VerificationForm";
 import "./index.css";
 
 function App() {
+  const [step, setStep] = useState(1);
   return (
-    <div className="App">
-    <VerificationForm />
+    <div className="flex justify-center">
+      <VerificationForm />
+
+      <div>
+        <h1>container</h1>
+        {step == 1 ? (
+          <div>step 1</div>
+        ) : step == 2 ? (
+          <div>step 2</div>
+        ) : step == 3 ? (
+          <div>step 3</div>
+        ) : null}
+        {step < 3 && (
+          <button onClick={() => setStep(step + 1)} className="p-3">
+            go
+          </button>
+        )}
+        {step <= 3 && (
+          <button onClick={() => setStep(step - 1)} className="p-3">
+            back
+          </button>
+        )}
+      </div>
     </div>
   );
 }
--- FrontEnd/src/components/Forms/VerificationForm.tsx
+++ FrontEnd/src/components/Forms/VerificationForm.tsx
@@ -1,16 +1,15 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { SetStateAction, useState, useRef } from "react";
-import * as Yup from "yup";
 const VerificationForm = () => {
   return (
     <div>
       <Formik
         initialValues={{
           age: 0,
-          VaccNumber:0,
+          VaccNumber: "",
           effected: "",
-          chronicDisease:"",
-          chronicDiseaseDetails:""
+          chronicDisease: "",
+          chronicDiseaseDetails: "",
         }}
         // validationSchema={UserSchema}
         onSubmit={(values) => {
@@ -18,7 +17,7 @@ const VerificationForm = () => {
         }}
       >
         {({ errors, touched, values }) => (
-          <Form>
+          <Form className="w-[23em]">
             {/* age */}
             <div className="mt-4">
               <label
@@ -34,69 +33,70 @@ const VerificationForm = () => {
             </div>
 
             {values.age > 12 && (
-              <div className="mt-4">
-                <label>
-                  <label htmlFor="">firstVacc</label>
-                  <Field 
-                    type="radio"  
-                    className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600" 
+              <div className="mt-4 flex gap-4">
+                <div className="flex items-center gap-3 bg-red-300 p-1 rounded-md">
+                  <Field
+                    type="radio"
                     name="VaccNumber"
-                    value={1} 
-                    defaultChecked={checked}
-                    onChange={setChecked(!checked)}
+                    className="w-3 h-3 cursor-pointer"
+                    value="1"
                   />
-                  <label htmlFor="">secondVacc</label>
-                  <Field 
-                    type="radio" 
-                    name="VaccNumber" 
-                    className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600" 
-                    value={2} 
+                  <label>First vaccine</label>
+                </div>
+                <div className="flex items-center gap-3">
+                  <Field
+                    type="radio"
+                    name="VaccNumber"
+                    className="w-3 h-3 cursor-pointer"
+                    value="2"
                   />
-                  <label htmlFor="">thirdVacc</label>
-                  <Field 
-                    type="radio" 
-                    name="VaccNumber" 
-                    className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600" 
-
-                    value={3} 
+                  <label>Second vaccine</label>
+                </div>
+                <div className="flex items-center gap-3">
+                  <Field
+                    type="radio"
+                    name="VaccNumber"
+                    className="w-3 h-3 cursor-pointer"
+                    value="3"
                   />
-                </label>  
+                  <label>third vaccine</label>
+                </div>
               </div>
             )}
 
-            { values.VaccNumber == 1 ?(
+            {values.VaccNumber == "1" ? (
               <div>
-                <Field id="country-option-1" type="radio" name="countries" value="USA" className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600" aria-labelledby="country-option-1" aria-describedby="country-option-1" checked />             
+                <h2>rass lwl</h2>
               </div>
-            ): values.VaccNumber == 2 ?(
+            ) : values.VaccNumber == "2" ? (
               <div>
                 <h2>rass tani</h2>
               </div>
-            ):values.VaccNumber == 3 ?(
+            ) : values.VaccNumber == "3" ? (
               <div>
                 <h3>rass talat</h3>
               </div>
-            ):null
-          }
+            ) : null}
 
             <div className="mt-8 flex justify-between">
               <button
-                type="submit"
-                className="w-[12em] text-green-900 bg-white border border-green-300 hover:bg-gray-100 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                type="button"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
               >
-                save
+                Submit
               </button>
               <button
-                type="submit"
-                className="w-[12em] text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                type="button"
+                className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
               >
-                cancel
+                Reset
               </button>
             </div>
           </Form>
         )}
       </Formik>
     </div>
+    
   );
 };
 export default VerificationForm;
